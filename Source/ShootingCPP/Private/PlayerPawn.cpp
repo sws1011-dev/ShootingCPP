72454c414e16f68ac357126c3b7bd96022d00f3b
wooseok <sws1011.dev@gmail.com>
2026-05-06 16:46:45 +0900

    feat: PlayerPawn 입력 바인딩 - SetupPlayerInputComponent 오버라이딩

--- Source/ShootingCPP/Private/PlayerPawn.cpp
+++ Source/ShootingCPP/Private/PlayerPawn.cpp
@@ -3,6 +3,7 @@
 
 #include "PlayerPawn.h"
 
+#include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "Components/BoxComponent.h"
 
@@ -59,4 +60,13 @@ void APlayerPawn::Tick(float DeltaTime)
 void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	UEnhancedInputComponent* eic = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (eic != nullptr)
+	{
+		eic->BindAction(iaHorizontal, ETriggerEvent::Triggered, this, &APlayerPawn::OnInputHorizontal);
+		eic->BindAction(iaHorizontal, ETriggerEvent::Completed, this, &APlayerPawn::OnInputHorizontal);eic->BindAction(iaHorizontal, ETriggerEvent::Triggered, this, &APlayerPawn::OnInputHorizontal);
+		eic->BindAction(iaVertical, ETriggerEvent::Triggered, this, &APlayerPawn::OnInputVertical);
+		eic->BindAction(iaVertical, ETriggerEvent::Completed, this, &APlayerPawn::OnInputVertical);
+	}
 }
