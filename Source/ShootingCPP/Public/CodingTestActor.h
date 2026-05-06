84fcff2874440cf77cc342760599c0c04fcf667f
wooseok <sws1011.dev@gmail.com>
2026-05-06 12:40:19 +0900

    feat: CodingTestActor 변수별 UPROPERTY 옵션 실습

--- Source/ShootingCPP/Public/CodingTestActor.h
+++ Source/ShootingCPP/Public/CodingTestActor.h
@@ -24,9 +24,21 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	
 	// 기본 자료형 변수
+	UPROPERTY(EditAnywhere) // 블루프린트 설정창 OK / 레벨에 배치된 액터(인스턴스) OK
 	int32 number1 = 10;
+	
+	UPROPERTY(VisibleAnywhere) // 값을 볼 수 있지만 수정 불가 -> 코드에서만 수정할 수 있고, 에디터에서는 참고용
 	float number2 = 3.14f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) // 블루프린트 이벤트그래프에서 읽기쓰기 모두 가능
+	int32 number3 = 30;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly) // 블루프린트 이벤트그래프에서 읽기전용
+	int32 number4 = 40;
+	
+	UPROPERTY(EditInstanceOnly) // 레벨에 배치된 액터 인스턴스에서만 수정 가능
 	FString name = TEXT("홍길동");
+	
+	UPROPERTY(EditDefaultsOnly) // 블루프린트 설정에서만 수정 가능
 	bool isReady = false;
-
 };
