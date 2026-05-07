9df2decb62700f39a5538343aeb760f61aaf4ea5
wooseok <sws1011.dev@gmail.com>
2026-05-07 15:12:58 +0900

    feat: EnemyFactory 클래스 생성(Actor 상속)

---
+++ Source/ShootingCPP/Private/EnemyFactory.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyFactory.h"
+
+
+// Sets default values
+AEnemyFactory::AEnemyFactory()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AEnemyFactory::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemyFactory::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
